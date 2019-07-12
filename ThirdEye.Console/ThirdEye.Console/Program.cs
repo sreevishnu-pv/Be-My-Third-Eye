5ba84dd1d79c4b374ed7258d86b6d5eedacf9285
Sreevishnu V <srev@microsoft.com>
2019-07-12 11:22:31 +0530

    added few images urls for testing

--- ThirdEye.Console/ThirdEye.Console/Program.cs
+++ ThirdEye.Console/ThirdEye.Console/Program.cs
@@ -6,6 +6,9 @@ namespace ThirdEye.Console
 {
     class Program
     {
+        //http://images.mid-day.com/images/2017/oct/Aishwarya-Amitabh-ee.jpg
+        //https://c8.alamy.com/comp/C11EJX/drummers-in-a-temple-thaipusam-festival-in-tenkasi-tamil-nadu-tamilnadu-C11EJX.jpg
+        //https://media.cntraveler.com/photos/596ceda1f9e37f55cb0bfaf4/master/pass/chicago-botantic-garden-GettyImages-95607462.jpg
         static void Main(string[] args)
         {
             System.Console.WriteLine("###################################");
@@ -17,7 +20,7 @@ namespace ThirdEye.Console
 
             Bootstraper.Initialize();
             var imageTagger = (IImageTagger)Bootstraper.ServiceProvider.GetService(typeof(IImageTagger));
-            var result = imageTagger.TagImage(imageUrl);
+            var result = imageTagger.TagImage(imageUrl).Result;
             System.Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
             System.Console.Read();
         }
